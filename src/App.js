ebefd2d4808f01a15af20da0e5b90bd518a6ed0d
Prem <sharmaprem651@gmail.com>
2022-10-10 23:57:09 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import TodoList from './TodoList';
+
 
 function App() {
+  const [inputList, setInputList] = useState("")
+  const [itemsList, setItemsList] =   useState([]);
+
+  const itemEventHandler = (e) => {
+    setInputList(e.target.value)
+  }
+
+
+  const listOfItems = () => {
+    setItemsList((oldItems)=>{
+      return [...oldItems, inputList]
+    })
+    setInputList("");
+  }
+
+
+  const  onClickHandler = (id) => {
+    console.log("id is", id)
+    setItemsList((oldArray)=>{
+      return oldArray.filter((arraylist, index)=>{
+        return index!==id;
+      })
+    });
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='todoListWrapper'>
+        <h1>Todo List</h1>
+        <form>
+          <div className='formGroup'>
+            <input type={'text'} value={inputList} placeholder="Add a Items" onChange={itemEventHandler}/>
+            <button type='button' onClick={listOfItems}>+</button>
+          </div>
+          <ul className='todoLists'>
+                {
+                  itemsList.map((itemListVal, index)=>{
+                      return <TodoList 
+                              itemVal={itemListVal} 
+                              id = {index}
+                              key={index}
+                              onClickBtn = {onClickHandler}
+                              />
+                  })
+                }             
+          </ul>   
+
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/TodoList.js
@@ -0,0 +1,9 @@
+const TodoList = (props) => {
+    return(
+        <>
+            <li id={props.id}>{props.itemVal} <span className="closeListItems" onClick={()=>{props.onClickBtn(props.id)}}>x</span></li>     
+        </>
+    )
+}
+
+export default TodoList;
